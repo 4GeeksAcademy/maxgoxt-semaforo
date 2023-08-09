a9b3d68066860ec007b61e2d7d464a078bb56760
MaxGoxt <135187058+MaxGoxt@users.noreply.github.com>
2023-08-09 18:04:59 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,22 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState} from "react";
 
 //create your first component
 const Home = () => {
+
+	const [color,setColor]=useState("")
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="">
+			<div className="bg-black"><div className="opacity-0">.</div></div>
+			<div className="rounded-5 bg-dark col-1 p-4 m-auto">
+				<div onClick={() => setColor("100 rojo")} className={"bg-danger rounded-circle opacity-" 
+				+ (color == "100 rojo" ? "100 rojo" : "25")}><h1 className="opacity-0">.</h1></div>
+				<div onClick={() => setColor("100 naranja")} className={"bg-warning rounded-circle opacity-" 
+				+ (color == "100 naranja" ? "100 naranja" : "25")}><h1 className="opacity-0">.</h1></div>
+				<div onClick={() => setColor("100 verde")} className={"bg-success rounded-circle opacity-" 
+				+ (color == "100 verde" ? "100 verde" : "25")}><h1 className="opacity-0">.</h1></div>
+			</div>
+		</div> 
 	);
 };
 
