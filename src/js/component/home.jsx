eed2b18b92d8eebe77895e8bbf063aa19017d4c6
MaxGoxt <135187058+MaxGoxt@users.noreply.github.com>
2023-08-09 18:19:59 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,15 +5,17 @@ const Home = () => {
 
 	const [color,setColor]=useState("")
 
+	let coso = {width: '20px', height: '50px'}
+	
 	return (
 		<div className="">
-			<div className="bg-black"><div className="opacity-0">.</div></div>
-			<div className="rounded-5 bg-dark col-1 p-4 m-auto">
-				<div onClick={() => setColor("100 rojo")} className={"bg-danger rounded-circle opacity-" 
+			<div className="bg-black m-auto" style={coso}><div className="opacity-0">.</div></div>
+			<div className="rounded-5 bg-dark col-1 p-1 m-auto">
+				<div onClick={() => setColor("100 rojo")} className={"bg-danger p-4 rounded-circle opacity-" 
 				+ (color == "100 rojo" ? "100 rojo" : "25")}><h1 className="opacity-0">.</h1></div>
-				<div onClick={() => setColor("100 naranja")} className={"bg-warning rounded-circle opacity-" 
+				<div onClick={() => setColor("100 naranja")} className={"bg-warning p-4 rounded-circle opacity-" 
 				+ (color == "100 naranja" ? "100 naranja" : "25")}><h1 className="opacity-0">.</h1></div>
-				<div onClick={() => setColor("100 verde")} className={"bg-success rounded-circle opacity-" 
+				<div onClick={() => setColor("100 verde")} className={"bg-success p-4 rounded-circle opacity-" 
 				+ (color == "100 verde" ? "100 verde" : "25")}><h1 className="opacity-0">.</h1></div>
 			</div>
 		</div> 
